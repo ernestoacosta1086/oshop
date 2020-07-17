19468ae73da6c20177c64cf2e8e532da3ae8391d
Ernesto A <ernesto.acosta1086@gmail.com>
2020-07-17 09:23:50 -0400

    Install and setup firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAkM7Wv0xUW9RIJ6rWQmUwTvc43PG6uTwk',
+    authDomain: 'oshop-3072f.firebaseapp.com',
+    databaseURL: 'https://oshop-3072f.firebaseio.com',
+    projectId: 'oshop-3072f',
+    storageBucket: 'oshop-3072f.appspot.com',
+    messagingSenderId: '994028716484',
+    appId: '1:994028716484:web:d1d4ec6bf6719ba0e5e491',
+    measurementId: 'G-J4Y9V4FJRK'
+  }
 };
 
 /*
