da62da3a127f581f90b46f294ca982ec367f67f6
Ernesto A <ernesto.acosta1086@gmail.com>
2020-07-20 09:36:16 -0400

    Adding async pipe to unsubscribe after use a observable necesary in firebase

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'bs-navbar',
+  selector: "bs-navbar",
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css'],
 })
 export class BsNavbarComponent {
-  user: firebase.User;
+  user$: Observable<firebase.User>;
 
   constructor(private afAuth: AngularFireAuth) {
-    afAuth.authState.subscribe(user => this.user = user);
+    this.user$ = afAuth.authState;
   }
 
   logout() {
