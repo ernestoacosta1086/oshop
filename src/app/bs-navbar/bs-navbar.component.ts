371a3cf932b29e80edd9485889ebfe1ce75dc0f3
Ernesto A <ernesto.acosta1086@gmail.com>
2020-07-20 10:04:37 -0400

    Using a service to separate responsability of method login and logout interacting with firebase

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,20 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { AngularFireAuth } from 'angularfire2/auth';
-import { Observable } from 'rxjs';
+import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
-  selector: "bs-navbar",
+  selector: 'app-bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css'],
 })
 export class BsNavbarComponent {
-  user$: Observable<firebase.User>;
-
-  constructor(private afAuth: AngularFireAuth) {
-    this.user$ = afAuth.authState;
-  }
+  constructor(public auth: AuthService) {}
 
   logout() {
-    this.afAuth.auth.signOut();
+    this.auth.logout();
   }
 }
