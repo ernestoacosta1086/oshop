9cb97ce6737b28f16ec2615741fd0401016aa772
Ernesto A <ernesto.acosta1086@gmail.com>
2020-07-22 08:40:40 -0400

    Fixing a bug uid cant be property of null

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -6,15 +6,19 @@ import { ActivatedRoute } from '@angular/router';
 import { AppUser } from './models/app-user';
 import { UserService } from './user.service';
 import 'rxjs/add/operator/switchMap';
-
+import 'rxjs/add/observable/of';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   user$: Observable<firebase.User>;
 
-  constructor(private userService: UserService, private afAuth: AngularFireAuth, private route: ActivatedRoute) {
+  constructor(
+    private userService: UserService,
+    private afAuth: AngularFireAuth,
+    private route: ActivatedRoute
+  ) {
     this.user$ = afAuth.authState;
   }
 
@@ -22,7 +26,7 @@ export class AuthService {
     let returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
     localStorage.setItem('returnUrl', returnUrl);
 
-    this.afAuth.auth.signInWithRedirect (new firebase.auth.GoogleAuthProvider());
+    this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
   }
 
   logout() {
@@ -30,7 +34,10 @@ export class AuthService {
   }
 
   get appUser$(): Observable<AppUser> {
-    return this.user$
-      .switchMap( user => this.userService.get(user.uid));
+    return this.user$.switchMap((user) => {
+      if (user) return this.userService.get(user.uid);
+
+      return Observable.of(null);
+    });
   }
 }
