fa91475140e9dc605ec986069cb089b834795879
Ernesto A <ernesto.acosta1086@gmail.com>
2020-07-24 09:33:33 -0400

    Adding Custom Validation using ng2-validation third party library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,6 +28,8 @@ import { ProductFormComponent } from './admin/product-form/product-form.componen
 import { CategoryService } from './category.service';
 import { FormsModule } from '@angular/forms';
 import { ProductService } from './product.service';
+import { CustomFormsModule } from 'ng2-validation';
+
 
 @NgModule({
   declarations: [
@@ -51,6 +53,7 @@ import { ProductService } from './product.service';
     AngularFireAuthModule,
     NgbModule,
     FormsModule,
+    CustomFormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent },
       { path: 'products', component: ProductsComponent },
