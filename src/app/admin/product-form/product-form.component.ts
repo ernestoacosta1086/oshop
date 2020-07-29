013a1165d7c5c9a74d2998423a486ac25969b553
Ernesto A <ernesto.acosta1086@gmail.com>
2020-07-29 08:34:26 -0400

    Editing product

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from 'src/app/category.service';
 import { ProductService } from 'src/app/product.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import 'rxjs/add/operator/take';
 
 @Component({
   selector: 'app-product-form',
@@ -10,13 +11,18 @@ import { Router } from '@angular/router';
 })
 export class ProductFormComponent implements OnInit {
   categories$;
+  product = {};
 
   constructor(
     private router: Router,
+    private route: ActivatedRoute,
     categoryService: CategoryService,
     private productService: ProductService
   ) {
     this.categories$ = categoryService.getCategories();
+
+    let id = this.route.snapshot.paramMap.get('id');
+    if (id) this.productService.get(id).take(1).subscribe(p => this.product = p);
   }
 
   save(product) {
