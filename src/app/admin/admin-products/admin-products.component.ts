cbef9a49fce46ef2fce2e54023c9a862c6b7dba0
Ernesto A <ernesto.acosta1086@gmail.com>
2020-07-30 08:30:16 -0400

    Extracting an Interface

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit, OnDestroy } from "@angular/core";
-import { ProductService } from "src/app/product.service";
-import { Subscription } from "rxjs";
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ProductService } from 'src/app/product.service';
+import { Subscription } from 'rxjs';
+import { Product } from 'src/app/models/product';
 
 @Component({
-  selector: "app-admin-products",
-  templateUrl: "./admin-products.component.html",
-  styleUrls: ["./admin-products.component.css"],
+  selector: 'app-admin-products',
+  templateUrl: './admin-products.component.html',
+  styleUrls: ['./admin-products.component.css'],
 })
 export class AdminProductsComponent implements OnInit, OnDestroy {
-  products: { title: string }[];
+  products: Product[];
   filteredProducts: any[];
   subscription: Subscription;
 
