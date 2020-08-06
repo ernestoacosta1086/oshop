6a7a0183d72d55af4e69ec860df579840b8a9262
Ernesto A <ernesto.acosta1086@gmail.com>
2020-08-06 00:32:07 -0400

    Displaying the Categories

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
+import { CategoryService } from '../category.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent {
   products$;
+  categories$;
 
-  constructor(productService: ProductService) {
-      this.products$ = productService.getAll();
-   }
-
+  constructor(
+    productService: ProductService,
+    categoryService: CategoryService
+  ) {
+    this.products$ = productService.getAll();
+    this.categories$ = categoryService.getAll();
+  }
 }
