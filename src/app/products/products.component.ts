016e7ea26ddb2114153d51d67ea1766147c00afd
Ernesto A <ernesto.acosta1086@gmail.com>
2020-08-06 16:13:02 -0400

    Filtering by category working good

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,21 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../product.service';
-import { CategoryService } from '../category.service';
+import { Component } from "@angular/core";
+import { ProductService } from "../product.service";
+import { CategoryService } from "../category.service";
+import { ActivatedRoute } from "@angular/router";
+import { Product } from "../models/product";
 
 @Component({
-  selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css'],
+  selector: "app-products",
+  templateUrl: "./products.component.html",
+  styleUrls: ["./products.component.css"],
 })
 export class ProductsComponent {
-  products$;
+  public products: Product[] = [];
+  filteredProducts: Product[] = [];
   categories$;
+  category: string;
 
   constructor(
+    route: ActivatedRoute,
     productService: ProductService,
     categoryService: CategoryService
   ) {
-    this.products$ = productService.getAll();
+    productService.getAll().subscribe((products) => {
+      this.products = products;
+
+      route.queryParamMap.subscribe((params) => {
+        this.category = params.get("category");
+  
+        this.filteredProducts = this.category
+          ? this.products.filter((p) => p.category === this.category)
+          : this.products;
+      });
+    });
+
     this.categories$ = categoryService.getAll();
+
+    
   }
 }
