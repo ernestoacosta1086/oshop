eaeff526927128928fb1e24e4391db33296dd5d0
Ernesto A <ernesto.acosta1086@gmail.com>
2020-08-12 00:13:25 -0400

    Refactoring: Moving the Responsibility to the

--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { Product } from './models/product';
 
 @Injectable({
   providedIn: 'root',
@@ -7,9 +8,26 @@ import { AngularFireDatabase } from 'angularfire2/database';
 export class ShoppingCartService {
   constructor(private db: AngularFireDatabase) {}
 
-  create() {
+  private create() {
     return this.db.list('/shopping-carts').push({
       dateCreated: new Date().getTime(),
     });
+  }
+
+  getCart(cartId: string) {
+    return this.db.object('/shopping-carts/' + cartId);
+  }
+
+  private getOrCreateCart() {
+    let cartId = localStorage.getItem('cartId');
+    if (!cartId) {
+      this.create().then((result) => {
+        localStorage.setItem('cartId', result.key);
+
+        return this.getCart(result.key);
+      });
+    } else {
+      return this.getCart(cartId);
+    }
   }
 }
