c6ac0f97d985ecbbe546bb89e5387f75e08015f1
Ernesto A <ernesto.acosta1086@gmail.com>
2020-08-12 01:23:07 -0400

    Adding products to the shoping cart

--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { Product } from './models/product';
+import 'rxjs/add/operator/take';
 
 @Injectable({
   providedIn: 'root',
@@ -17,13 +19,24 @@ export class ShoppingCartService {
     return this.db.object('/shopping-carts/' + cartId);
   }
 
-  private async getOrCreateCart() {
+  private async getOrCreateCartId() {
     let cartId = localStorage.getItem('cartId');
-    if (!cartId) {
-      let result = await this.create();
-      localStorage.setItem('cartId', result.key);
-      return this.getCart(result.key);
-    }
-    return this.getCart(cartId);
+    if (cartId) return cartId;
+
+    let result = await this.create();
+    localStorage.setItem('cartId', result.key);
+    return result.key;
+  }
+
+  private getItem(cartId: string, productId: string) {
+    return this.db.object('/shopping-carts/' + cartId + '/items/' + productId);
+  }
+
+  async addToCart(product: Product) {
+    let cartId = await this.getOrCreateCartId();
+    let item$ = this.getItem(cartId, product.$key);
+    item$.take(1).subscribe((item: any) => {
+      item$.update({ product: product, quantity: (item.quantity || 0) + 1 });
+    });
   }
 }
