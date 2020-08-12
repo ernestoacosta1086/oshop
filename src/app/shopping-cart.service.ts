3e068c8c7381551889f51517699a32e83b4e1f75
Ernesto A <ernesto.acosta1086@gmail.com>
2020-08-12 12:36:20 -0400

    Displaying the Quantity

--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -15,11 +15,12 @@ export class ShoppingCartService {
     });
   }
 
-  getCart(cartId: string) {
+  async getCart() {
+    let cartId = await this.getOrCreateCartId();
     return this.db.object('/shopping-carts/' + cartId);
   }
 
-  private async getOrCreateCartId() {
+  private async getOrCreateCartId(): Promise<string> {
     let cartId = localStorage.getItem('cartId');
     if (cartId) return cartId;
 
