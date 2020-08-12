7713e7d33ae1ea106170e7cc192776ed0f2b5437
Ernesto A <ernesto.acosta1086@gmail.com>
2020-08-12 00:01:42 -0400

    Creating a Shopping Cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,6 +32,7 @@ import { CustomFormsModule } from 'ng2-validation';
 import { DataTableModule } from 'angular5-data-table';
 import { ProductFilterComponent } from './products/product-filter/product-filter.component';
 import { ProductCardComponent } from './product-card/product-card.component';
+import { ShoppingCartService } from './shopping-cart.service';
 
 @NgModule({
   declarations: [
@@ -48,7 +49,7 @@ import { ProductCardComponent } from './product-card/product-card.component';
     LoginComponent,
     ProductFormComponent,
     ProductFilterComponent,
-    ProductCardComponent
+    ProductCardComponent,
   ],
   imports: [
     BrowserModule,
@@ -103,7 +104,15 @@ import { ProductCardComponent } from './product-card/product-card.component';
       },
     ]),
   ],
-  providers: [AuthService, AuthGuard, AdminAuthGuard, UserService, CategoryService, ProductService],
+  providers: [
+    AuthService,
+    AuthGuard,
+    AdminAuthGuard,
+    UserService,
+    CategoryService,
+    ProductService,
+    ShoppingCartService
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Product } from '../models/product';
+import { ShoppingCartService } from '../shopping-cart.service';
 
 @Component({
   selector: 'app-product-card',
@@ -10,6 +11,19 @@ export class ProductCardComponent {
   @Input() product: Product;
   @Input('show-actions') showActions = true;
 
-  constructor() { }
+  constructor(private cartService: ShoppingCartService) { }
+
+  addToCart(product: Product) {
+    let cartId = localStorage.getItem('cartId');
+    if(!cartId) {
+      this.cartService.create().then(result => {
+        localStorage.setItem('cartId', result.key);
+
+        //Add product to cart
+      });
+    } else {
+      //Add product to cart
+    }
+  }
 
 }
