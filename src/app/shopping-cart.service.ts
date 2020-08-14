931af7ea627190496e2ca9e7ff4ec74083defcea
Ernesto A <ernesto.acosta1086@gmail.com>
2020-08-14 12:43:19 -0400

    Clearing the Shopping Cart

--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -15,17 +15,30 @@ import { Observable } from 'rxjs';
 export class ShoppingCartService {
   constructor(private db: AngularFireDatabase) {}
 
-  private create() {
-    return this.db.list('/shopping-carts').push({
-      dateCreated: new Date().getTime(),
-    });
-  }
-
   async getCart(): Promise<Observable<ShoppingCart>> {
     let cartId = await this.getOrCreateCartId();
     return this.db
       .object('/shopping-carts/' + cartId)
       .map((x: any) => new ShoppingCart(x.items));
+  }
+
+  async addToCart(product: Product) {
+    this.updateItem(product, 1);
+  }
+
+  async removeFromCart(product: Product) {
+    this.updateItem(product, -1);
+  }
+
+  async clearCart() {
+    let cartId = await this.getOrCreateCartId();
+    this.db.object('/shopping-carts/' + cartId + '/items').remove();
+  }
+
+  private create() {
+    return this.db.list('/shopping-carts').push({
+      dateCreated: new Date().getTime(),
+    });
   }
 
   private async getOrCreateCartId(): Promise<string> {
@@ -41,14 +54,6 @@ export class ShoppingCartService {
     return this.db.object('/shopping-carts/' + cartId + '/items/' + productId);
   }
 
-  addToCart(product: Product) {
-    this.updateItem(product, 1);
-  }
-
-  removeFromCart(product: Product) {
-    this.updateItem(product, -1);
-  }
-
   private async updateItem(product: Product, change: number) {
     let cartId = await this.getOrCreateCartId();
     let item$ = this.getItem(cartId, product.$key);
@@ -60,5 +65,5 @@ export class ShoppingCartService {
         quantity: (item.quantity || 0) + change,
       });
     });
-  }
+  }  
 }
